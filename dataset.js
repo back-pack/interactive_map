2bb05722f0b7fe8a710484b287590aa6842e4f2a
Oscar <oscar.eber@gmail.com>
2019-04-28 20:29:15 -0300

    first commit

---
+++ dataset.js
@@ -0,0 +1,29 @@
+
+var dataset = 
+[
+    {
+        "Titulo": "City Tours",
+        "Direccion": {
+            "PuntoA": { "lat": "123123", "long": "123123", "Direccion": "Calle blabla 123", },
+            "Direccion2": { "lat": "123123", "long": "123123"},
+            "Direccion3": { "lat": "123123", "long": "123123"}
+        },
+    },
+    {
+        "Titulo": "CongregacionesDeSoporte",
+        "Direccion":  {
+            "Direccion1": { "lat": "123123", "long": "123123"},
+            "Direccion2": { "lat": "123123", "long": "123123"},
+            "Direccion3": { "lat": "123123", "long": "123123"}
+        }
+        
+    },
+    {
+        "Titulo": "Recorridos",
+        "Direccion": {
+            "Direccion1": { "lat": "123123", "long": "123123"},
+            "Direccion2": { "lat": "123123", "long": "123123"},
+            "Direccion3": { "lat": "123123", "long": "123123"}
+        }
+    }
+];
