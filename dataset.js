19c126be94555ff8f1610d8ff19b588d423affdf
unknown <marianofreddi@gmail.com>
2019-05-02 14:38:59 -0300

    draft of json

--- dataset.js
+++ dataset.js
@@ -1,5 +1,5 @@
 
-var dataset = 
+var dataset =
 [
     {
         "Titulo": "City Tours",
@@ -16,12 +16,20 @@ var dataset =
             "Direccion2": { "lat": "123123", "long": "123123"},
             "Direccion3": { "lat": "123123", "long": "123123"}
         }
-        
+
     },
     {
-        "Titulo": "Recorridos",
+        "Titulo": "Asistencia Temaiken",
         "Direccion": {
-            "Direccion1": { "lat": "123123", "long": "123123"},
+            "Nombre": { "Nombre" : "Salón del Reino, ZONA GENERAL microcentro CABA.",
+                        "Direccion" : "Tucumán 2024. Congregación anfitriona: SAN TELMO",
+                        "lat": "-58.396021",
+                        "long": "-34.602159",
+                        "Link maps" : "https://www.google.com.ar/maps/place/Tucum%C3%A1n+2024,+C1050AAP+CABA/@-34.6023872,-58.3981926,17z/data=!3m1!4b1!4m5!3m4!1s0x95bccaeac88af511:0x48ba412e73323f47!8m2!3d-34.6023916!4d-58.3960039"
+                        "Contacto de emergencia" : "Leandro Federico Musri",
+                        "Telefono" : "(011) 15-5622-4730"
+                        "Mail" : "CONG03191@jwpub.org" },
+
             "Direccion2": { "lat": "123123", "long": "123123"},
             "Direccion3": { "lat": "123123", "long": "123123"}
         }
