127c19fa988c0ecbda38e98f07e2a8807201cdba
Oscar <oscar.eber@gmail.com>
2019-05-14 10:55:45 -0300

    changing map source

--- datamap.js
+++ datamap.js
@@ -24,9 +24,9 @@
     // style URL
     style: 'mapbox://styles/mapbox/light-v10',
     // initial position in [long, lat] format
-    center: [-58.4649283, -34.6278461],
+    center: [-58.4803213, -34.6026808],
     // initial zoom
-    zoom: 10,
+    zoom: 11,
     scrollZoom: true
   });
   
@@ -284,10 +284,18 @@
     // This is where your '.addLayer()' used to be, instead add only the source without styling a layer
     map.addSource("places", {
       "type": "geojson",
-      "data": stores
-    });
+      "data": {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-58.4803213, -34.6026808]
+        },
+        "properties": {
+            "title": "Mapbox DC",
+            "marker-symbol": "monument"
+        }
+    }});
     // Initialize the list
-    buildLocationList(stores);
     buildMainLocationList(dataset);
   });
 
@@ -306,7 +314,7 @@
 
     el.addEventListener('click', function(e){
         // 1. Fly to the point
-        flyToStore(marker);
+        flyToLocation(marker);
 
         // 2. Close all other popups and display popup for clicked store
         createPopUp(marker);
@@ -326,20 +334,20 @@
   });
 
 
-  function flyToStore(currentFeature) {
+  function flyToLocation(currentFeature) {
     map.flyTo({
         center: currentFeature.coordinates,
         zoom: 15
       });
   }
 
-  function createPopUp(currentFeature) {
+  function createPopUp(currentLocations) {
     var popUps = document.getElementsByClassName('mapboxgl-popup');
     if (popUps[0]) popUps[0].remove();
 
 
     var popup = new mapboxgl.Popup({closeOnClick: false})
-          .setLngLat(currentFeature.coordinates)
+          .setLngLat(currentLocations.coordinates)
         //   .setHTML('<h3>Sweetgreen</h3>' +
         //     '<h4>' + currentFeature.properties.address + '</h4>')
           .addTo(map);
@@ -372,16 +380,17 @@
             divItem = document.createElement('div');
             divItem.className = "item";
             link.dataPosition = i;
+            link.dataLocation = j;
             divItem.appendChild(link);
             divCollapse.appendChild(divItem);
 
 
             link.addEventListener('click', function(e) {
                 // Update the currentFeature to the store associated with the clicked link
-                var clickedListing = dataset[this.dataPosition].data;
-
+                var clickedListing = dataset[this.dataPosition].data[this.dataLocation];
+                console.log(clickedListing);
                 // 1. Fly to the point
-                flyToStore(clickedListing);
+                flyToLocation(clickedListing);
 
                 // 2. Close all other popups and display popup for clicked store
                 createPopUp(clickedListing);
@@ -400,52 +409,4 @@
         divAccordion.appendChild(divCollapse);
     } 
 
-  }
-
-  function buildLocationList(data) {
-    console.log(dataset);
-
-    for (i = 0; i < data.features.length; i++) {
-      var currentFeature = data.features[i];
-      var prop = currentFeature.properties;
-
-      var listings = document.getElementById('listings');
-      var listing = listings.appendChild(document.createElement('div'));
-      listing.className = 'item';
-      listing.id = "listing-" + i;
-
-      var link = listing.appendChild(document.createElement('a'));
-      link.href = '#';
-      link.className = 'title';
-      link.dataPosition = i;
-      link.innerHTML = prop.address;
-
-      var details = listing.appendChild(document.createElement('div'));
-      details.innerHTML = prop.city;
-      if (prop.phone) {
-        details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-      }
-
-
-
-      link.addEventListener('click', function(e){
-        // Update the currentFeature to the store associated with the clicked link
-        var clickedListing = data.features[this.dataPosition];
-
-        // 1. Fly to the point
-        flyToStore(clickedListing);
-
-        // 2. Close all other popups and display popup for clicked store
-        createPopUp(clickedListing);
-
-        // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-        var activeItem = document.getElementsByClassName('active');
-
-        if (activeItem[0]) {
-           activeItem[0].classList.remove('active');
-        }
-        this.parentNode.classList.add('active');
-        });
-
-    }
   }
